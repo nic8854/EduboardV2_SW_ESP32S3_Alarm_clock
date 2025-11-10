71519c4f08cda00e94ef1a420db7b48ce28fe5d0
nic8854 <40262536+nic8854@users.noreply.github.com>
2025-11-10 22:35:38 +0100

    implement smoothe changing of time

--- src/main.c
+++ src/main.c
@@ -19,9 +19,9 @@
 #define UPDATETIME_MS 100
 
 typedef struct {
-    uint8_t hours;
-    uint8_t minutes;
-    uint8_t seconds;
+    int8_t hours;
+    int8_t minutes;
+    int8_t seconds;
 } clockTime_t;
 
 clockTime_t currentTime;
@@ -34,10 +34,11 @@ clockTime_t currentTime;
 #define SET_SECONDS_BIT     (1 << 7)  // bit 7
 
 EventGroupHandle_t alarmClockEventGroup;
-
+QueueHandle_t rotationQueue;
 
 
 void timeTask(void* param) {
+    uint16_t timeChange = 0;
     currentTime.hours = 0;
     currentTime.minutes = 0;
     currentTime.seconds = 0;
@@ -46,20 +47,52 @@ void timeTask(void* param) {
         EventBits_t currentBits = xEventGroupGetBits(alarmClockEventGroup);
         if((currentBits & SET_TIME_BIT) == 0) {
             currentTime.seconds++;
-        }
-        if(currentTime.seconds >= 60) {
-            currentTime.seconds = 0;
-            currentTime.minutes++;
-        }
-        if(currentTime.minutes >= 60) {
-            currentTime.minutes = 0;
-            currentTime.hours++;
-        }
-        if(currentTime.hours >= 24) {
-            currentTime.hours = 0;
+            if(currentTime.seconds >= 60) {
+                currentTime.seconds = 0;
+                currentTime.minutes++;
+            }
+            if(currentTime.minutes >= 60) {
+                currentTime.minutes = 0;
+                currentTime.hours++;
+            }
+            if(currentTime.hours >= 24) {
+                currentTime.hours = 0;
+            }
+            vTaskDelay(1000/portTICK_PERIOD_MS);
+        } else {
+            if(xQueueReceive(rotationQueue, &timeChange, portMAX_DELAY) == pdTRUE) {
+                if((currentBits & SET_HOURS_BIT)) {
+                     currentTime.hours += timeChange;
+                     timeChange = 0;
+                     if(currentTime.hours >= 24) {
+                        currentTime.hours = 0;
+                     }
+                     if(currentTime.hours <= -1) {
+                        currentTime.hours = 23;
+                     }
+                } else if((currentBits & SET_MINUTES_BIT)) {
+                    currentTime.minutes += timeChange;
+                    timeChange = 0;
+                    if(currentTime.minutes >= 60) {
+                       currentTime.minutes = 0;
+                    }
+                    if(currentTime.minutes <= -1) {
+                       currentTime.minutes = 59;
+                    }
+                } else if((currentBits & SET_SECONDS_BIT)) {
+                    currentTime.seconds += timeChange;
+                    timeChange = 0;
+                    if(currentTime.seconds >= 60) {
+                       currentTime.seconds = 0;
+                    }
+                    if(currentTime.seconds <= -1) {
+                       currentTime.seconds = 59;
+                    }
+                }
+            }
+            vTaskDelay(50/portTICK_PERIOD_MS);
         }
         ESP_LOGI(TAG, "current time = %d:%d:%d", currentTime.hours, currentTime.minutes, currentTime.seconds);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
     }
 }
 
@@ -74,7 +107,13 @@ void displayTask(void* param) {
         lcdDrawRect(xpos-10, ypos+5, xpos+140, ypos-35, BLUE);
         lcdDrawString(fx32L, xpos, ypos, &displayTime[0], color);
         lcdUpdateVScreen();
-        vTaskDelay(200/portTICK_PERIOD_MS);
+        EventBits_t currentBits = xEventGroupGetBits(alarmClockEventGroup);
+        if((currentBits & SET_TIME_BIT) == 0) {
+            vTaskDelay(500/portTICK_PERIOD_MS);
+        } else {
+            vTaskDelay(50/portTICK_PERIOD_MS);
+        }
+        
     }
 }
 
@@ -105,46 +144,61 @@ void inputTask(void* param) {
             switch(timeSetMode) {
                 case SET_HOURS: {
                     if(rotary_encoder_button_get_state(false) == SHORT_PRESSED) {
+                        xEventGroupClearBits(alarmClockEventGroup, SET_HOURS_BIT);
+                        xEventGroupSetBits(alarmClockEventGroup, SET_MINUTES_BIT);
+                        led_set(LED0, 0);
+                        led_set(LED1, 1);
                         timeSetMode = SET_MINUTES;
                         ESP_LOGI(TAG, "mode = SET_MINUTES");
                         rotationChange = 0;
                         break;
                     } else {
-                        rotationChange += rotary_encoder_get_rotation(true);
                         ESP_LOGI(TAG, "SET_HOURS change --> %d", rotationChange);
                         break;
                     }
                 }
                 case SET_MINUTES: {
                     if(rotary_encoder_button_get_state(false) == SHORT_PRESSED) {
+                        xEventGroupClearBits(alarmClockEventGroup, SET_MINUTES_BIT);
+                        xEventGroupSetBits(alarmClockEventGroup, SET_SECONDS_BIT);
+                        led_set(LED1, 0);
+                        led_set(LED2, 1);
                         timeSetMode = SET_SECONDS;
                         ESP_LOGI(TAG, "mode = SET_SECONDS");
                         rotationChange = 0;
                         break;
                     } else {
-                        rotationChange += rotary_encoder_get_rotation(true);
                         ESP_LOGI(TAG, "SET_MINUTES change --> %d", rotationChange);
                         break;
                     }
                 }
                 case SET_SECONDS: {
                     if(rotary_encoder_button_get_state(false) == SHORT_PRESSED) {
+                        xEventGroupClearBits(alarmClockEventGroup, SET_SECONDS_BIT);
+                        xEventGroupSetBits(alarmClockEventGroup, SET_HOURS_BIT);
+                        led_set(LED0, 1);
+                        led_set(LED2, 0);
                         timeSetMode = SET_HOURS;
                         ESP_LOGI(TAG, "mode = SET_HOURS");
                         rotationChange = 0;
                         break;
                     } else {
-                        rotationChange += rotary_encoder_get_rotation(true);
                         ESP_LOGI(TAG, "SET_SECONDS change --> %d", rotationChange);
                         break;
                     }
                 }
             }
+
+            rotationChange = rotary_encoder_get_rotation(true);
+            if(rotationChange != 0) {
+                xQueueSend(rotationQueue, &rotationChange, 0);
+                rotationChange = 0;
+            }
         } else {
+            xEventGroupSetBits(alarmClockEventGroup, SET_HOURS_BIT);
             timeSetMode = SET_HOURS;
         }
         rotary_encoder_button_get_state(true);
-        // delay
         vTaskDelay(50/portTICK_PERIOD_MS);
     }
 }
@@ -156,6 +210,7 @@ void app_main()
     eduboard2_init();
 
     alarmClockEventGroup = xEventGroupCreate();
+    rotationQueue = xQueueCreate(10, sizeof(int32_t));
     
     //Create timeTask
     xTaskCreate(timeTask, "timeTask", 2*2048, NULL, 10, NULL);
